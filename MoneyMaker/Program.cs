09959757214e0e12897014f346e1f3984f124dbd
Tuğrul Sübekci <tugrulsubekci@gmail.com>
2022-05-21 15:49:11 +0300

    i finished the second project with c#

--- MoneyMaker/Program.cs
+++ MoneyMaker/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Let the user know that the program is starting:
+Console.WriteLine("Welcome to Money Maker!");
+
+// Let the user give you a value and then convert it to double
+Console.WriteLine("Enter an amount to convert to coins:");
+string totalMoney = Console.ReadLine();
+double myTotalMoney = Convert.ToDouble(totalMoney);
+Console.WriteLine($"{myTotalMoney} cents is equal to...");
+
+//Convert to coins
+int goldCoin = 10;
+int silverCoin = 5;
+
+double goldCoins = Math.Floor(myTotalMoney / goldCoin);
+double leftOverGold = myTotalMoney % goldCoin;
+
+double silverCoins = Math.Floor(leftOverGold / silverCoin);
+double leftOverSilver = leftOverGold % silverCoin;
+
+//print the results
+Console.WriteLine($"Gold coins: {goldCoins}\nSilver coins: {silverCoins}\nBronze coins: {leftOverSilver}");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MoneyMaker
+What is the minimum number of coins that equals 98 cents? You can find the answer with this program. :)
+The project is so simple. This is my 2nd project with C#.
